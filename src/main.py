6193ce69d3399a82e962b2010973d8f0f9565f69
hameem76 <hameem.ismail.sn@gmail.com>
2024-11-15 17:58:41 +0000

    Updated

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 import json
 
 import git.exc
-import yaml
-from copy import deepcopy
-from typing import Any
+#import yaml
 
 from git import Repo
 import os
@@ -26,7 +24,6 @@ class Services:
     STATIC_CONTENT= "static_content"
 
 
-services_discovered : dict[str, Any]= {}
 
 def find_between( s, first, last ):
     try:
@@ -36,104 +33,140 @@ def find_between( s, first, last ):
     except ValueError:
         return ""
 
-def parse_from_docker_file(file_content: str):
-    services_discovered[Services.DOCKER] = {}
-    for line in file_content.split("\n"):
-        if 'CMD' in line:
-            if 'npm' in line:
-                services_discovered[Services.DOCKER][Services.APP_SERVER] = "Nodejs"
-            elif 'flask' in line:
-                services_discovered[Services.DOCKER][Services.APP_SERVER] = "Flask"
-            elif 'django' in line:
-                services_discovered[Services.DOCKER][Services.APP_SERVER] = 'Django'
-            elif 'start-kafka' in line:
-                services_discovered[Services.DOCKER][Services.MQ] = 'kafka'
-            elif 'rabbitmq' in line:
-                services_discovered[Services.DOCKER][Services.MQ] = 'rabbitmq'
+class GitHubDisovery():
+    def __init__(self):
+        self.services_discovered = {}
 
-    return
+    def parse_from_docker_file(self, file_content: str):
+        services_discovered = self.services_discovered
+        services_discovered[Services.DOCKER] = {}
+        for line in file_content.split("\n"):
+            if 'CMD' in line:
+                if 'npm' in line:
+                    services_discovered[Services.DOCKER][Services.APP_SERVER] = "Nodejs"
+                elif 'flask' in line:
+                    services_discovered[Services.DOCKER][Services.APP_SERVER] = "Flask"
+                elif 'django' in line:
+                    services_discovered[Services.DOCKER][Services.APP_SERVER] = 'Django'
+                elif 'start-kafka' in line:
+                    services_discovered[Services.DOCKER][Services.MQ] = 'kafka'
+                elif 'rabbitmq' in line:
+                    services_discovered[Services.DOCKER][Services.MQ] = 'rabbitmq'
+        print(self.services_discovered)
+        return
 
 
-def parse_from_docker_compose(file_content: str):
-    services_discovered[Services.DOCKER] = {}
+    def parse_from_docker_compose(self, file_content: str):
+        self.services_discovered[Services.DOCKER] = {}
 
 
-def parse_from_nginx_conf(file_content: str):
-    start = "upstream backend {"
-    end = "}"
-    server_entries = find_between(file_content, start, end)
-    servers_count =0
-    servers = []
-    for entry in server_entries.split("\n"):
-        if entry.strip().startswith("server"):
-            servers_count += 1
-            servers.append(entry.strip().split(" ")[1])
-    services_discovered[Services.LOAD_BALANCER] = {"server_count": servers_count, "servers": servers}
-    return
+    def parse_from_nginx_conf(self, file_content: str):
+        start = "upstream backend {"
+        end = "}"
+        server_entries = find_between(file_content, start, end)
+        servers_count =0
+        servers = []
+        for entry in server_entries.split("\n"):
+            if entry.strip().startswith("server"):
+                servers_count += 1
+                servers.append(entry.strip().split(" ")[1])
+        self.services_discovered[Services.LOAD_BALANCER] = {"server_count": servers_count, "servers": servers}
+        return
 
-def parse_from_package_json(file_content: str):
-    json_dict = json.loads(file_content)
-    services_discovered[Services.APP_SERVER] = "node.js"
-    dependencies = json_dict.get("dependencies")
-    discovery_map = [
-        {
-            "search_service" : Services.DATABASE,
-            "searchable_keywords": ["mysql", "mongodb", "postgres"]
-        },
-        {
-            "search_service": Services.CACHE,
-            "searchable_keywords": ["redis"]
-        },
-    ]
+    def parse_from_package_json(self, file_content: str):
+        json_dict = json.loads(file_content)
+        services_discovered = self.services_discovered
+        services_discovered[Services.APP_SERVER] = "node.js"
+        dependencies = json_dict.get("dependencies")
+        discovery_map = [
+            {
+                "search_service" : Services.DATABASE,
+                "searchable_keywords": ["mysql", "mongodb", "postgres"]
+            },
+            {
+                "search_service": Services.CACHE,
+                "searchable_keywords": ["redis"]
+            },
+        ]
 
-    if dependencies:
-        for discovery_entry in discovery_map:
-            search_service = discovery_entry['search_service']
-            searchable_keywords = discovery_entry['searchable_keywords']
-            discovered_entities = services_discovered.get(search_service, [])
-            for key in dependencies.keys():
-                for keyword in searchable_keywords:
-                    if keyword in key:
-                        discovered_entities.append(keyword)
-            services_discovered[search_service] = discovered_entities
-    return
+        if dependencies:
+            for discovery_entry in discovery_map:
+                search_service = discovery_entry['search_service']
+                searchable_keywords = discovery_entry['searchable_keywords']
+                discovered_entities = services_discovered.get(search_service, [])
+                for key in dependencies.keys():
+                    for keyword in searchable_keywords:
+                        if keyword in key:
+                            discovered_entities.append(keyword)
+                services_discovered[search_service] = discovered_entities
+        return
 
-def parse_from_requirements_txt(file_content: str):
-    discovery_map = [
-        {
-            "search_service" : Services.DATABASE,
-            "searchable_keywords": {"mysql": "mysqlclient" , "mongodb": "pymongo", "postgres" : "postgres"}
-        },
-        {
-            "search_service": Services.CACHE,
-            "searchable_keywords": {"redis": "redis"}
-        },
-    ]
-    lib_entries = file_content.split("\n")
-    for entry in lib_entries:
-        for discovery_entry in discovery_map:
-            search_service = discovery_entry['search_service']
-            searchable_keywords = discovery_entry['searchable_keywords']
-            discovered_entities = services_discovered.get(search_service, [])
-            for key,val in searchable_keywords.items():
-                if val in entry.split("==")[0]:
-                    discovered_entities.append(key)
-            services_discovered[search_service] = discovered_entities
-    return
+    def parse_from_requirements_txt(self, file_content: str):
+        services_discovered = self.services_discovered
+        discovery_map = [
+            {
+                "search_service" : Services.DATABASE,
+                "searchable_keywords": {"mysql": "mysqlclient" , "mongodb": "pymongo", "postgres" : "postgres"}
+            },
+            {
+                "search_service": Services.CACHE,
+                "searchable_keywords": {"redis": "redis"}
+            },
+        ]
+        lib_entries = file_content.split("\n")
+        for entry in lib_entries:
+            for discovery_entry in discovery_map:
+                search_service = discovery_entry['search_service']
+                searchable_keywords = discovery_entry['searchable_keywords']
+                discovered_entities = services_discovered.get(search_service, [])
+                for key,val in searchable_keywords.items():
+                    if val in entry.split("==")[0]:
+                        discovered_entities.append(key)
+                services_discovered[search_service] = discovered_entities
+        return
 
-def parse_from_py_files(file_content: str):
-    # Fetch from boto3 client access for AWS.
-    # Similarly fetch for Azure services as well with appropriate key_words with respective cloud call
-    boto3_services = {Services.AWS_S3: "s3", Services.AWS_SQS: "sqs", Services.AWS_SNS: "sns", Services.AWS_RDS: "rds",
-                      Services.AWS_LAMBDA: "lambda", Services.AWS_CLOUDTRAIL: "cloudtrail"}
-    for key, val in boto3_services.items():
-        if f"boto3.client('{val}')" in file_content or f'boto3.client("{val}")' in file_content:
-            services_discovered[key] = "enabled"
+    def parse_from_py_files(self, file_content: str):
+        services_discovered = self.services_discovered
+        # Fetch from boto3 client access for AWS.
+        # Similarly fetch for Azure services as well with appropriate key_words with respective cloud call
+        boto3_services = {Services.AWS_S3: "s3", Services.AWS_SQS: "sqs", Services.AWS_SNS: "sns", Services.AWS_RDS: "rds",
+                          Services.AWS_LAMBDA: "lambda", Services.AWS_CLOUDTRAIL: "cloudtrail"}
+        for key, val in boto3_services.items():
+            if f"boto3.client('{val}')" in file_content or f'boto3.client("{val}")' in file_content:
+                services_discovered[key] = "enabled"
 
-    return
+        return
 
-def parse_from_js_files(file_content: str):
-    return
+    def parse_from_js_files(self, file_content: str):
+        return
+
+
+# List all files in the repository
+    def discover_services_in_repo(self, directory):
+        #files = []
+        services_discovered = self.services_discovered
+        for root, _, filenames in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(root, filename)
+                if filename == 'Dockerfile':
+                    file_content = read_file(file_path)
+                    self.parse_from_docker_file(file_content)
+                elif filename.lower() == 'package.json':
+                    file_content = read_file(file_path)
+                    self.parse_from_package_json(file_content)
+                elif filename.lower() == 'requirements.txt':
+                    file_content = read_file(file_path)
+                    self.parse_from_requirements_txt(file_content)
+                elif filename.endswith(".py"):
+                    file_content = read_file(file_path)
+                    self.parse_from_py_files(file_content)
+                elif filename == 'load-balancer.conf':
+                    file_content = read_file(file_path)
+                    self.parse_from_nginx_conf(file_content)
+                services_discovered[Services.STATIC_CONTENT] = "enabled"
+
+                #files.append(file_path)
+        return services_discovered
 
 # Clone a repository
 def clone_repo(url, directory):
@@ -142,33 +175,6 @@ def clone_repo(url, directory):
         print(f"Cloned repository to {directory}")
     else:
         print(f"Repository already exists at {directory}")
-
-# List all files in the repository
-def discover_services_in_repo(directory):
-    #files = []
-    services_discovered = {}
-    for root, _, filenames in os.walk(directory):
-        for filename in filenames:
-            file_path = os.path.join(root, filename)
-            if filename == 'Dockerfile':
-                file_content = read_file(file_path)
-                parse_from_docker_file(file_content)
-            elif filename.lower() == 'package.json':
-                file_content = read_file(file_path)
-                parse_from_package_json(file_content)
-            elif filename.lower() == 'requirements.txt':
-                file_content = read_file(file_path)
-                parse_from_requirements_txt(file_content)
-            elif filename.endswith(".py"):
-                file_content = read_file(file_path)
-                parse_from_py_files(file_content)
-            elif filename == 'load-balancer.conf':
-                file_content = read_file(file_path)
-                parse_from_nginx_conf(file_content)
-            services_discovered[Services.STATIC_CONTENT] = "enabled"
-
-            #files.append(file_path)
-    return services_discovered
 
 # Read the content of a file
 def read_file(filepath):
@@ -218,12 +224,6 @@ def build_aws_architecture(customerA_services, customerB_services) -> str:
             architecture_content_dict["Diagram"]["Resources"]["EC2_1"] = ec2
             vpc["Children"].append("EC2_1")
 
-        # if services_discovered.get(Services.DOCKER):
-        #     ec2 = {
-        #         "Type" : "AWS::EC2::Instance"
-        #     }
-        #     architecture_content_dict["Diagram"]["Resources"]["EC2_1"] = ec2
-
         if _services_discovered.get(Services.AWS_SQS):
             sqs = {
                 "Type": "AWS::SQS"
@@ -244,9 +244,9 @@ def build_aws_architecture(customerA_services, customerB_services) -> str:
                 "Type": "AWS::S3"
             }
             architecture_content_dict["Diagram"]["Resources"]["S3"] = s3
-            aws_cloud["Children"].append("SNS")
+            aws_cloud["Children"].append("S3")
 
-    print(architecture_content_dict)
+    #print(architecture_content_dict)
     return json.dumps(architecture_content_dict)
 
 
@@ -261,7 +261,9 @@ def m1(customerArepo, customerBrepo):
         print(f"Git repo does not exist {customerArepo}")
         raise Exception(f"Git repo does not exist {customerArepo}")
 
-    customerA = deepcopy(discover_services_in_repo(repo_directory))
+    #customerA = deepcopy(discover_services_in_repo(repo_directory))
+    customerA = GitHubDisovery().discover_services_in_repo(repo_directory)
+    #print("CustomerA", customerA)
     repo_directory = "/tmp/testrepos/customerB"#'/home/hameem/git_repos/hameem76/test_repos/customerB'  # Replace with the desired directory
     try:
         clone_repo(customerBrepo, repo_directory)
@@ -269,7 +271,8 @@ def m1(customerArepo, customerBrepo):
         print(f"Git repo does not exist {customerBrepo}")
         raise Exception(f"Git repo does not exist {customerBrepo}")
 
-    customerB = deepcopy(discover_services_in_repo(repo_directory))
+    customerB = GitHubDisovery().discover_services_in_repo(repo_directory)
+    #print("CustomerB", customerA)
     #print("discovery", services_discovered)
     #print(customerA)
     architecture_content = build_aws_architecture(customerA, customerB) 
@@ -283,11 +286,12 @@ def m1(customerArepo, customerBrepo):
 
 if __name__ == '__main__':
     # Buiild the UI
-    standalone = False
+    standalone = True
     if standalone:
-        customerA_url = "https://github.com/hameem76/hameem76//test_repo/customerA"#st.text_input("CustomerA Repo URL")
-        customerB_url =  "https://github.com/hameem76/hameem76//test_repo/customerB"#st.text_input("CustomerB repo URL")
-        m1(customerA_url, customerB_url)
+        customerA_url = "https://github.com/hameem76/test_repo_customerA"#st.text_input("CustomerA Repo URL")
+        customerB_url =  "https://github.com/hameem76/test_repo_customerB"#st.text_input("CustomerB repo URL")
+        architecture = m1(customerA_url, customerB_url)
+        #print(architecture)
 
     else:
         st.title("Discover Services from Git repos")
